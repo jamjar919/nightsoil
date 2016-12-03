ded4bb3f3bf1136f5109b10ee5145bd9a3447992
jamjar919 <james@mjsquare.com>
2016-12-03 14:59:39 +0000

    update data

--- data/scrape.py
+++ data/scrape.py
@@ -18,10 +18,11 @@ def strip_tags(html):
     s.feed(html)
     return s.get_data()
 
+posts = list();
+
 for pagenumber in range(1,10):
     r = requests.get('http://a.4cdn.org/s4s/'+str(pagenumber)+'.json')
     data = json.loads(r.text);
-    posts = list();
     for thread in data["threads"]:
         threadnumber = thread["posts"][0]["no"];
         r = requests.get('http://a.4cdn.org/s4s/thread/'+str(threadnumber)+'.json')
@@ -35,5 +36,5 @@ for pagenumber in range(1,10):
             except KeyError:
                 print("image only post, skipping");
 
-with open('s4s.json', 'w') as fp:
+with open('s4s.json', 'w', encoding="utf-8") as fp:
     json.dump(posts, fp)
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var mark = require('markovify.js');
 var rshi = require('./data/rshitpost.json');
+var s4s = require('./data/s4s.json');
 
 var reddit_goods = [];
 
@@ -7,6 +8,6 @@ for (i in rshi.data.children){
 	reddit_goods.push(rshi.data.children[i].data.title);
 }
 
-var c = new mark.Chain(reddit_goods);
+var c = new mark.Chain(s4s["data"]);
 var x = mark.markov(c);
 console.log(x);
