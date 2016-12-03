89d4cd4e4fae44789378b1c75dbffa73dad8ab09
jamjar919 <james@mjsquare.com>
2016-12-03 15:35:09 +0000

    add html decode

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
 var mark = require('markovify.js');
 var rshi = require('./data/rshitpost.json');
 var four = require('./data/s4s.json');
-
+var strings = require('locutus/php/strings/')
 var messages = []
 
-// for (i in rshi.data.children){
-// 	messages.push(rshi.data.children[i].data.title);
-// }
-for (i in four.k){
-	messages.push(four.k[i]);
-}
-
-
-var c = new mark.Chain(messages);
+var c = new mark.Chain(four);
 var x = mark.markov(c);
-console.log(x);
+console.log(strings.html_entity_decode(x));
